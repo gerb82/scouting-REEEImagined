99482fac62ad722822f1cfa66ff0dbfa8c49da8a
gerb82 <licrishveshocko@gmail.com>
2018-11-11 17:53:23 +0200

    worked on ActionHandler

--- src/utilities/GBSockets/ActionHandler.java
+++ src/utilities/GBSockets/ActionHandler.java
@@ -4,25 +4,72 @@ import java.util.HashMap;
 
 public class ActionHandler {
 
-    public interface PacketHandler {}
+    private enum PacketHandler {
+        STANDARD{
+            void handle(GBSocket socket, Packet packet){
+                ((StandardMethod)method).hanlde(socket, packet.content, packet.contentType);
+            }
 
-    public interface standard extends PacketHandler{
-        void testMethod(GBSocket socket, Object content, String contentType, String packetType);
+            PacketHandler create(AbstractPacketHandler method){
+                this.method = method;
+                return this;
+            }
+        },
+        STANDARD_WITH_IDS{
+            void handle(GBSocket socket, Packet packet){
+                ((StandardMethodWithIDs)method).hanlde(socket, packet.content, packet.contentType, packet.ids);
+            }
+
+            PacketHandler create(AbstractPacketHandler method){
+                this.method = method;
+                return this;
+            }
+        },
+        UNSAFE{
+            void handle(GBSocket socket, Packet packet){
+                ((UnsafeMethod)method).hanlde(socket, packet);
+            }
+
+            PacketHandler create(AbstractPacketHandler method){
+                this.method = method;
+                return this;
+            }
+        };
+        AbstractPacketHandler method;
+        abstract void handle(GBSocket socket, Packet packet);
+        abstract PacketHandler create(AbstractPacketHandler method);
+    }
+
+    public interface AbstractPacketHandler{
+    }
+
+    public interface StandardMethod extends AbstractPacketHandler{
+        void hanlde(GBSocket socket, Object content, String contentType);
+    }
+
+    public interface UnsafeMethod extends AbstractPacketHandler{
+        void hanlde(GBSocket socket, Packet packet);
+    }
+
+    public interface StandardMethodWithIDs extends AbstractPacketHandler{
+        void hanlde(GBSocket socket, Object content, String contentType, int[] IDs);
     }
 
     private HashMap<String, PacketHandler> handlers;
 
-    public static <T extends PacketHandler> void addAction(T action, String packetType){
-
+    public void addHandler(String packetType, StandardMethod method){
+        handlers.put(packetType, PacketHandler.STANDARD.create(method));
     }
 
-    private static void testMeothod(String hi){
-
+    public void addHandler(String packetType, StandardMethodWithIDs method){
+        handlers.put(packetType, PacketHandler.STANDARD_WITH_IDS.create(method));
     }
 
-    {
-        addAction(ActionHandler::testMeothod, "test");
+    public void addHandler(String packetType, UnsafeMethod method){
+        handlers.put(packetType, PacketHandler.UNSAFE.create(method));
     }
 
-
+    protected void handlePacket(GBSocket socket, Packet packet) throws BadPacketException{
+        handlers.get(packet.packetType).handle(socket, packet);
+    }
 }
--- src/utilities/GBSockets/BadPacketException.java
+++ src/utilities/GBSockets/BadPacketException.java
@@ -5,4 +5,11 @@ public class BadPacketException extends Exception {
     public BadPacketException(String message){
         super(message);
     }
+
+    public BadPacketException(String message, Packet packet){
+        super(message);
+        this.packet = packet;
+    }
+
+    private Packet packet;
 }
