35687067892570619eb2765b3104c2205e290452
gerb82 <licrishveshocko@gmail.com>
2018-11-12 08:43:10 +0200

    "Fixed" ActionHandler I recommend against trying to change/understand it Trust me It works, and that's all that counts (I'm so gonna regret this when writing the javadoc)

--- src/utilities/GBSockets/ActionHandler.java
+++ src/utilities/GBSockets/ActionHandler.java
@@ -1,75 +1,108 @@
 package utilities.GBSockets;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 public class ActionHandler {
 
-    private enum PacketHandler {
-        STANDARD{
-            void handle(GBSocket socket, Packet packet){
-                ((StandardMethod)method).hanlde(socket, packet.content, packet.contentType);
+    public interface HandlerType{}
+
+    private class Handlers{
+
+        private PacketHandler handler;
+        private Class<? extends HandlerType> handlerType;
+
+        private Handlers(PacketHandler handler, Class<? extends HandlerType> handlerType){
+            this.handler = handler;
+            this.handlerType = handlerType;
+        }
+    }
+
+    public class StandardHandler implements HandlerType{
+
+        private Object content;
+        private String contentType;
+        private GBSocket socket;
+
+        public StandardHandler(GBSocket socket, Packet packet){
+            content = packet.content;
+            contentType = packet.contentType;
+            this.socket = socket;
+        }
+
+        public Object getContent() {
+            return content;
+        }
+
+        public String getContentType() {
+            return contentType;
+        }
+
+        public GBSocket getSocket() {
+            return socket;
+        }
+    }
+
+    public class StandardHandlerWithIDs extends StandardHandler{
+
+        private int[] ids;
+
+        public StandardHandlerWithIDs(GBSocket socket, Packet packet) {
+            super(socket, packet);
+            ids = packet.ids;
+        }
+
+        public int[] getIds() {
+            return ids;
+        }
+    }
+
+    public class UnsafeHandler implements HandlerType{
+
+        private Packet packet;
+        private GBSocket socket;
+
+        public UnsafeHandler(GBSocket socket, Packet packet){
+            this.packet = packet;
+            this.socket = socket;
+        }
+
+        public Packet getPacket() {
+            return packet;
+        }
+
+        public GBSocket getSocket() {
+            return socket;
+        }
+    }
+
+    public interface PacketHandler{
+        void handle(HandlerType handler);
+    }
+
+    private HashMap<String, Handlers> handlers;
+
+    public void addMethod(String packetType, PacketHandler handler, Class<? extends HandlerType> handlerType){
+        handlers.put(packetType, new Handlers(handler, handlerType));
+    }
+
+    protected void handlePacket(GBSocket socket, Packet packet) throws BadPacketException {
+        if(handlers.containsKey(packet.packetType)){
+            try {
+                Handlers handler = handlers.get(packet.packetType);
+                handler.handler.handle(handler.handlerType.getDeclaredConstructor(GBSocket.class, Packet.class).newInstance(socket, packet));
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
             }
-
-            PacketHandler create(AbstractPacketHandler method){
-                this.method = method;
-                return this;
-            }
-        },
-        STANDARD_WITH_IDS{
-            void handle(GBSocket socket, Packet packet){
-                ((StandardMethodWithIDs)method).hanlde(socket, packet.content, packet.contentType, packet.ids);
-            }
-
-            PacketHandler create(AbstractPacketHandler method){
-                this.method = method;
-                return this;
-            }
-        },
-        UNSAFE{
-            void handle(GBSocket socket, Packet packet){
-                ((UnsafeMethod)method).hanlde(socket, packet);
-            }
-
-            PacketHandler create(AbstractPacketHandler method){
-                this.method = method;
-                return this;
-            }
-        };
-        AbstractPacketHandler method;
-        abstract void handle(GBSocket socket, Packet packet);
-        abstract PacketHandler create(AbstractPacketHandler method);
-    }
-
-    public interface AbstractPacketHandler{
-    }
-
-    public interface StandardMethod extends AbstractPacketHandler{
-        void hanlde(GBSocket socket, Object content, String contentType);
-    }
-
-    public interface UnsafeMethod extends AbstractPacketHandler{
-        void hanlde(GBSocket socket, Packet packet);
-    }
-
-    public interface StandardMethodWithIDs extends AbstractPacketHandler{
-        void hanlde(GBSocket socket, Object content, String contentType, int[] IDs);
-    }
-
-    private HashMap<String, PacketHandler> handlers;
-
-    public void addHandler(String packetType, StandardMethod method){
-        handlers.put(packetType, PacketHandler.STANDARD.create(method));
-    }
-
-    public void addHandler(String packetType, StandardMethodWithIDs method){
-        handlers.put(packetType, PacketHandler.STANDARD_WITH_IDS.create(method));
-    }
-
-    public void addHandler(String packetType, UnsafeMethod method){
-        handlers.put(packetType, PacketHandler.UNSAFE.create(method));
-    }
-
-    protected void handlePacket(GBSocket socket, Packet packet) throws BadPacketException{
-        handlers.get(packet.packetType).handle(socket, packet);
+        }
+        else{
+            throw new BadPacketException("The packet type was invalid. No action handler for this packet type exists.");
+        }
     }
 }
