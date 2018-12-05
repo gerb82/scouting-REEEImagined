f9808f7dd6b94b6ce3c40388aa3d83450076bc79
gerb82 <licrishveshocko@gmail.com>
2018-12-05 23:42:18 +0200

    Made skeleton for the scouting REEE-Imagined v0.1

---
+++ src/scouterSide/MainLogic.java
@@ -0,0 +1,17 @@
+package scouterSide;
+
+public class MainLogic {
+
+    // connect to server
+
+
+
+
+    // sync up with game
+
+
+
+
+
+    // manage UI
+}
---
+++ src/serverSide/code/ServerManager.java
@@ -0,0 +1,20 @@
+package serverSide.code;
+
+public class ServerManager {
+
+    // accept connection
+
+
+
+
+
+
+
+    // share vids directory with http
+
+
+
+
+
+    // send relevant data to client for syncing on a specific game
+}
