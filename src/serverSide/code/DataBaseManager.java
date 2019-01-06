f2e86ef3a109a789de742c8f7c056290d3a5385d
gerb82 <licrishveshocko@gmail.com>
2019-01-07 00:38:18 +0200

    more work on the DataBaseManager. once it's finished only a bit of connection syncing needs to happen, and then we'll have everything. also, huge thanks to daniel(i really hope i remembered your name correctly, i suck at names ;-;) for coming here today, and helping me clear my mind on a lot of things, as well as being a hype-partner for the system, and see you again soon hopefully :D.

--- src/serverSide/code/DataBaseManager.java
+++ src/serverSide/code/DataBaseManager.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DataBaseManager implements Closeable {
 
@@ -29,8 +31,8 @@ public class DataBaseManager implements Closeable {
         teamNumbers, teamNames, participatedIn, // teams table
         gameNumbers, mapConfiguration, competition, teamNumber1, teamNumber2, teamNumber3, teamNumber4, teamNumber5, teamNumber6, // games table
         competitionID, competitionName, // competitions
-        eventAbsoluteID, eventChainID, eventLocationInChain, eventType, timeStamps, // stamps table
-        chainID, gameNumber, competitionNumber, teamNumber, startingLocation // main events table
+        eventChainID, eventLocationInChain, eventType, timeStamps, // stamps table
+        chainID, gameNumber, competitionNumber, teamNumber, alliance, startingLocation // main events table
     }
 
     private enum tables{
@@ -47,19 +49,20 @@ public class DataBaseManager implements Closeable {
             Statement statement = database.createStatement();
 
             // eventDefinitionsList
-            statement.execute("CREATE TABLE IF NOT EXISTS " + tables.eventTypes + "(" +
-                    columns.eventTypeID + " integer primary key," + System.lineSeparator() +
+            statement.execute("CREATE TABLE IF NOT EXISTS " + tables.eventTypes + "(" + System.lineSeparator() +
+                    columns.eventTypeID + " integer primary key AUTO INCREMENT," + System.lineSeparator() +
                     columns.eventName + " text NOT NULL UNIQUE);");
 
             // competitions
             statement.execute("CREATE TABLE IF NOT EXISTS " + tables.competitions + "(" + System.lineSeparator() +
-                    columns.competitionID + " integer primary key," + System.lineSeparator() +
+                    columns.competitionID + " integer primary key AUTO INCREMENT," + System.lineSeparator() +
                     columns.competitionName + " text NOT NULL);");
 
             // containableEventsList
             statement.execute("CREATE TABLE IF NOT EXISTS "+ tables.containableEvents + "(" + System.lineSeparator() +
-                    columns.containerEventType + " integer NOT NULL," + System.lineSeparator() +
-                    columns.eventContainedType + " integer NOT NULL," + System.lineSeparator() +
+                    columns.containerEventType + " integer," + System.lineSeparator() +
+                    columns.eventContainedType + " integer," + System.lineSeparator() +
+                    "PRIMARY KEY(" + columns.containerEventType + "," + columns.eventContainedType + ")." + System.lineSeparator() +
                     "FOREIGN KEY(" + columns.containerEventType + ") REFERENCES " + tables.eventTypes + "(" + columns.eventTypeID + ")," + System.lineSeparator() +
                     "FOREIGN KEY(" + columns.eventContainedType + ") REFERENCES " + tables.eventTypes + "(" + columns.eventTypeID + "));");
 
@@ -90,21 +93,22 @@ public class DataBaseManager implements Closeable {
 
             // eventFrames
             statement.execute("CREATE TABLE IF NOT EXISTS " + tables.eventFrames + "(" + System.lineSeparator() +
-                    columns.chainID + " integer primary key," + System.lineSeparator() +
+                    columns.chainID + " integer primary key AUTO INCREMENT," + System.lineSeparator() +
                     columns.gameNumber + " integer NOT NULL," + System.lineSeparator() +
                     columns.competitionNumber + " integer NOT NULL," + System.lineSeparator() +
-                    columns.teamNumber + " integer NOT NULL," + System.lineSeparator() +
+                    columns.teamNumber + " integer," + System.lineSeparator() +
+                    columns.alliance + " integer NOT NULL," + System.lineSeparator() +
                     columns.startingLocation + " integer NOT NULL," + System.lineSeparator() +
                     "FOREIGN KEY(" + columns.gameNumber + "," + columns.competitionNumber + ") REFERENCES " + tables.games + "(" + columns.gameNumbers + "," + columns.competition + ")," + System.lineSeparator() +
                     "FOREIGN KEY(" + columns.teamNumber + ") REFERENCES " + tables.teamNumbers + "(" + columns.teamNumbers + "));");
 
             // eventsList
             statement.execute("CREATE TABLE IF NOT EXISTS " + tables.events + "(" + System.lineSeparator() +
-                    columns.eventAbsoluteID + " integer primary key," + System.lineSeparator() +
-                    columns.eventChainID + " integer NOT NULL," + System.lineSeparator() +
-                    columns.eventLocationInChain + " integer NOT NULL," + System.lineSeparator() +
+                    columns.eventChainID + " integer ," + System.lineSeparator() +
+                    columns.eventLocationInChain + " integer," + System.lineSeparator() +
                     columns.eventType + " integer NOT NULL," + System.lineSeparator() +
                     columns.timeStamps + " integer," + System.lineSeparator() +
+                    "PRIMARY KEY(" + columns.eventChainID + "," + columns.eventLocationInChain + ")," + System.lineSeparator() +
                     "FOREIGN KEY(" + columns.eventType + ") REFERENCES " + tables.eventTypes + "(" + columns.eventTypeID + ")," + System.lineSeparator() +
                     "FOREIGN KEY(" + columns.eventChainID + ") REFERENCES " + tables.eventFrames + "(" + columns.chainID + "));");
             database.commit();
@@ -139,8 +143,8 @@ public class DataBaseManager implements Closeable {
             String signUpTeams = "UPDATE " + tables.teamNumbers + " SET " + columns.participatedIn + name + " = 1 WHERE ";
             boolean first = true;
             for (ScoutedTeam team : teams) {
-                signUpTeams += (!first ? " OR " : "") + columns.teamNumbers  + " = " + team.getNumber();
                 insertTeams += (!first ? "," : "") + "(" + team.getNumber() + ",'" + team.getName() + "'" + "," + "1)";
+                signUpTeams += (!first ? " OR " : "") + columns.teamNumbers  + " = " + team.getNumber();
                 first = false;
             }
             statement.execute(insertTeams + ";");
@@ -174,14 +178,29 @@ public class DataBaseManager implements Closeable {
         }
     }
 
-    protected void updateEvents(FullScoutingEvent[] events){
+    protected void updateEvents(int game, String competition, FullScoutingEvent[] events){
         try(Statement statement = database.createStatement()){
-            String inserter = "INSERT OR REPLACE INTO " + tables.eventFrames + "(" + columns.chainID + "," + columns.gameNumber + "," + columns.competitionNumber + "," + columns.teamNumber + "," + columns.startingLocation + ") values";
+            Set<Integer> numberedEvents = new HashSet<>();
+            boolean first = true;
+            String idList = "(";
+            for(FullScoutingEvent event : events){
+                if(event.getEvent().getChainID() != -1){
+                    if(numberedEvents.contains(event.getEvent().getChainID())){
+                        throw new IllegalArgumentException("A duplicate event was detected!");
+                    } else {
+                        numberedEvents.add(event.getEvent().getChainID());
+                        idList += (!first ? "," : "") + event.getEvent().getChainID();
+                    }
+                }
+            }
+            // delete all relevant stamps. delete all event frames that aren't in the new list. insert or replace with new frames. add the new stamps. newest chain id is loaded from the database by getting the highest 1 chain id right now and then assigning them in an increasing size to the frames.
+            String framesFixer = "DELETE FROM " + tables.events + " WHERE " + columns.eventChainID + " in " + ("SELECT " + columns.chainID + " from " + tables.eventFrames + " where " + columns.gameNumber + " = " + game + " AND " + columns.competitionNumber + " = " + ("SELECT " + columns.competitionID + " from " + tables.competitions + " where " + columns.competitionName + " = " + competition)) + ")";
+            String framesInserter = "INSERT INTO " + tables.eventFrames + "(" + columns.chainID + "," + columns.gameNumber + "," + columns.competitionNumber + "," + columns.teamNumber + "," + columns.startingLocation + ") values";
             String stampsFixer = "DELETE FROM " + tables.events + " WHERE ";
             String stampsInserter = "INSERT INTO " + tables.events + "(" + columns.eventChainID + "," + columns.eventLocationInChain + "," + columns.eventType + "," + columns.timeStamps + ") values";
             boolean first = true;
             for(FullScoutingEvent event : events){
-                inserter += (!first ? "," : "") + "(" + event.getEvent().getChainID() + "," + event.getGame() + "," + event.getCompetition() + "," + event.getTeam() + "," + event.getStartingLocation() + ")";
+                framesInserter += (!first ? "," : "") + "(" + event.getEvent().getChainID() + "," + event.getGame() + "," + event.getCompetition() + "," + event.getTeam() + "," + event.getAlliance() + "," + event.getStartingLocation() + ")";
                 stampsFixer += (!first ? " OR " : "") + columns.eventChainID + " = " + event.getEvent().getChainID();
                 int location = 0;
                 for(ScoutingEvent.EventTimeStamp stamp : event.getEvent().getStamps()){
@@ -189,7 +208,7 @@ public class DataBaseManager implements Closeable {
                     first = false;
                 }
             }
-            statement.execute(inserter + ";" + stampsFixer + ";" + stampsInserter + ";");
+            statement.execute(framesInserter + "; " + stampsFixer + "; " + stampsInserter + ";");
             database.commit();
         } catch (SQLException e){
             try {
@@ -201,9 +220,36 @@ public class DataBaseManager implements Closeable {
         }
     }
 
-    protected void addGame(int gameNumber, String mapConfiguration, )
+    protected void addGame(int gameNumber, String competition, String mapConfiguration, Integer[] teams){
+        try (Statement statement = database.createStatement()){
+            assert (teams.length == 6);
+            ArrayList<Integer> testList = new ArrayList<>();
+            for(Integer teamNum : teams){
+                if(!testList.contains(teamNum) || teamNum == null){
+                    testList.add(teamNum);
+                } else {
+                    throw new IllegalArgumentException("The same team was supplied twice for the given new game!");
+                }
+            }
+            String gameCreate = "INSERT INTO " + tables.games + "(" + columns.gameNumbers + "," + columns.competition + "," + columns.mapConfiguration + "," + columns.teamNumber1 + "," + columns.teamNumber2 + "," + columns.teamNumber3 + "," + columns.teamNumber4 + "," + columns.teamNumber5 + "," + columns.teamNumber6 + ") values(" + gameNumber + "," + "(Select " + columns.competitionID + " from " + tables.competitions + " where " + columns.competitionName + " = '" + competition + "')" + "," + mapConfiguration;
+            for(Integer teamNum : teams){
+                gameCreate += "," + teamNum;
+            }
+            statement.execute(gameCreate + ";");
+            database.commit();
+        } catch (AssertionError e){
+            throw new IllegalArgumentException("The teams array was not of the right size!");
+        } catch (SQLException e) {
+            try {
+                database.rollback();
+            } catch (SQLException e1) {
+                throw new Error("Could not roll back database changes!");
+            }
+            throw new IllegalArgumentException("The transaction could not be completed.", e);
+        }
+    }
 
-
+    protected void
 
 
 
